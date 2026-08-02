d8e3003caade249aa817dce7d024884fb4fb2b09
micool35250-lgtm <micool35250@gmail.com>
2026-08-02 23:55:06 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+/*
+  Synchronisation familiale Firebase.
+  L'application fonctionne sans configuration en mode local.
+  Après création du projet Firebase, remplacez null par l'objet de configuration fourni.
+*/
+window.ROADTRIP_FIREBASE_CONFIG = null;
+window.ROADTRIP_FAMILY_CODE = "dib-europe-2026";
