b24d8e6d041f9bcccff4538f1900a27b9cb9882b
micool35250-lgtm <micool35250@gmail.com>
2026-08-03 03:04:37 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,19 @@
 /*
-  Synchronisation familiale Firebase.
-  L'application fonctionne sans configuration en mode local.
-  Après création du projet Firebase, remplacez null par l'objet de configuration fourni.
+  Synchronisation familiale RoadTrip Dib.
+  Configuration Firebase Web du projet roadtrip-dib-2026.
+  Les quatre iPhone utilisent le même document Firestore pour partager
+  la progression et les badges.
 */
-window.ROADTRIP_FIREBASE_CONFIG = null;
+window.ROADTRIP_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB9uy0VqavEprCJCxym2-gHNVXW8eMytdU",
+  authDomain: "roadtrip-dib-2026.firebaseapp.com",
+  projectId: "roadtrip-dib-2026",
+  storageBucket: "roadtrip-dib-2026.firebasestorage.app",
+  messagingSenderId: "235884188746",
+  appId: "1:235884188746:web:28f507bdd7a79a6362c2b3",
+  measurementId: "G-BH3Q98MQWQ"
+};
+
 window.ROADTRIP_FAMILY_CODE = "dib-europe-2026";
+
+window.ROADTRIP_FAMILY_EMAIL = "road.trip.2026@famille.dibbo";
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="roadtrip-v6-sos-tab";
+const CACHE="roadtrip-v7-family-login";
 const CORE=["./","./index.html","./manifest.webmanifest","./firebase-config.js","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
